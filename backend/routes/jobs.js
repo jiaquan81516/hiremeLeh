60645fa48c3179240fcbcb288dd3322148875829
jiaquan81516 <103324308+jiaquan81516@users.noreply.github.com>
2026-04-30 20:31:53 +0800

    Update jobs.js

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getJobs, getTrends, getSkillGap } = require('../controllers/jobsController');
-
+const { getJobs, getTrends, getSkillGap, getTopSkills } = require('../controllers/jobsController');
 router.get('/', getJobs);
 router.get('/trends', getTrends);
 router.get('/skillgap', getSkillGap);
